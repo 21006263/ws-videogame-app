376c875d806c5a9b21fdc3adaeb0ebdbd7f58bed
Henry <hmorenosifontes@gmail.com>
2022-03-12 13:06:28 -0400

    Nuevo

--- api/index.js
+++ api/index.js
@@ -26,34 +26,39 @@ const { conn, /*Genre*/ } = require('./src/db.js');
 //!datos para mantener prepargas
 
 
-// Syncing all the models at once.
-conn.sync({ force: false }).then( /*async*/ () => {
+// // Syncing all the models at once.
+// conn.sync({ force: false }).then( /*async*/ () => {
 
-// //Precargar los generos
-// const verification = await Genre.findAll()
-// if(verification.length <1){
-// //Tendre que caragr 
+// // //Precargar los generos
+// // const verification = await Genre.findAll()
+// // if(verification.length <1){
+// // //Tendre que caragr 
 
 
-// // todo          https://api.rawg.io/api/genres?key=5232972c76f74429ab954178fce13d1b
-// const pedido = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
+// // // todo          https://api.rawg.io/api/genres?key=5232972c76f74429ab954178fce13d1b
+// // const pedido = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
 
-//   const formateo = pedido.data.results?.map(gn => {
-//   return {
+// //   const formateo = pedido.data.results?.map(gn => {
+// //   return {
   
-//     name: gn.name
-//    //
+// //     name: gn.name
+// //    //
     
-//   }
-// })
+// //   }
+// // })
 
-// //Buklkcreate Recibe una regla de objetos y crea una fila por cada uno 
-// await Genre.bulkCreate(formateo)
-// //console.log(formateo)
+// // //Buklkcreate Recibe una regla de objetos y crea una fila por cada uno 
+// // await Genre.bulkCreate(formateo)
+// // //console.log(formateo)
 
-// }
+// // }
 
-  server.listen(process.env.PORT , () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+//   server.listen(process.env.PORT , () => {
+//     console.log('%s listening at 3001'); // eslint-disable-line no-console
+//   });
+// });
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
 });
