21cdffc01b1bb653d71f6f861addb380e8be4aec
Henry <hmorenosifontes@gmail.com>
2022-03-12 12:19:19 -0400

    add

--- api/src/app.js
+++ api/src/app.js
@@ -3,14 +3,14 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.routes');
-
+const cors = require("cors");
 require('./db.js');
 
 const server = express();
-
-server.name = 'API';
-const cors = require("cors");
 server.use(cors());
+server.name = 'API';
+
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
